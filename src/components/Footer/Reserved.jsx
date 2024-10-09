c2e5b76c1a610b158bcc350e82ed0a61073ed59f
kramislife <egariando619@gmail.com>
2024-10-09 16:37:22 +0800

    Update Reserved.jsx footer with author name

--- src/components/Footer/Reserved.jsx
+++ src/components/Footer/Reserved.jsx
@@ -4,8 +4,8 @@ const Reserved = () => {
   return (
     <div className="flex lg:mx-14 lg:px-0 px-3 py-4 border-t border-neutral-800/90">
       <div className="flex justify-center lg:justify-between w-full">
-        <p className="text-neutral-500 text-xs tracking-wide">
-          &copy; 2024 Elysium VR. All rights reserved.
+        <p className="text-neutral-500 text-xs tracking-wide text-center">
+          &copy; 2024 Elysium VR. All rights reserved. Coded by Mark Gariando
         </p>
 
         <div className="hidden lg:flex items-center lg:gap-2">
