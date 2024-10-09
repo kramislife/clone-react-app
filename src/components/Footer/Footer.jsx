b4a056e8b8683d6f31ad4c7473e924370c25d368
kramislife <egariando619@gmail.com>
2024-10-09 16:17:20 +0800

    Refactor Footer component and GetStarted component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,89 +4,44 @@ import Socials from "./Socials";
 import Reserved from "./Reserved";
 import GetStarted from "./GetStarted";
 
-import { socials } from "../../constant/Index";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Button } from "../ui/button";
-
 const Footer = () => {
   return (
     <footer>
-      <div className="bg-background2 py-10 px-4 border-t border-neutral-700/80">
-        <div className="flex flex-col">
-          <div className="flex items-center lg:justify-start justify-center lg:px-12 gap-2 lg:mb-0 mb-10">
-            <img src={logo} alt="logo" className="h-12 w-12" />
-            <p className="gradient text-xl tracking-tighter">Elysium VR </p>
+      <div className="bg-background2 py-10 px-6 lg:px-14 border-t border-neutral-700/80">
+        <div className="flex justify-center lg:justify-start">
+          <div className="flex items-center gap-2">
+            <img src={logo} alt="" className="h-16 w-16" />
+            <span className="gradient text-xl tracking-tighter">
+              Elysium VR
+            </span>
           </div>
-          <div className="grid grid-cols-2 lg:flex lg:flex-row lg:space-x-24 lg:gap-14 lg:ml-14 lg:pt-10 lg:pb-5 gap-5">
-            {footer.map((item) => (
-              <div key={item.id} className="space-y-4 lg:p-0 pl-8 py-5">
-                <h6 className="text-2xl lg:text-sm text-neutral-500 font-semibold">
-                  {item.title}
-                </h6>
-                <ul className="lg:space-y-2 space-y-1">
-                  {item.links.map((link) => (
-                    <li key={link.id} className="lg:py-0 py-2">
-                      <a
-                        className="lg:text-sm text-md font-extralight text-neutral-300"
-                        href={link.path}
-                      >
-                        {link.title}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ))}
-
-            <div className="flex flex-col items-center relative w-full lg:max-w-sm">
-              <div className=" space-y-4">
-                <Label
-                  htmlFor="email"
-                  className="text-md font-semibold text-neutral-500 tracking-tight"
-                >
-                  Try Elysium VR for free
-                </Label>
-                <div className="border rounded-md border-neutral-700/80">
-                  <Input
-                    type="email"
-                    placeholder="Enter your email"
-                    className="bg-foreground lg:w-full lg:border-none w-full"
-                  />
-                  <Button
-                    className="absolute flex bg-orange-600 rounded-md rounded-l-none top-10 lg:right-0 right-0"
-                    type="submit"
-                  >
-                    Get Started
-                  </Button>
-                </div>
-              </div>
-              <div className="flex flex-col space-y-4 mt-5">
-                <div>
-                  <h6 className="gradient font-semibold text-lg tracking-tight">
-                    Connect with us
-                  </h6>
-                </div>
-                <div className="flex space-x-5">
-                  {socials.map((social, index) => (
+        </div>
+        <div className="flex flex-wrap gap-10 justify-around lg:justify-start lg:gap-10 lg:space-x-24 pt-10">
+          {footer.map((item) => (
+            <div key={item.id} className="pl-3">
+              <h6 className="text-lg tracking-tighter text-neutral-500 font-semibold">
+                {item.title}
+              </h6>
+              <ul className="pt-4">
+                {item.links.map((link) => (
+                  <li key={link.id} className="py-2">
                     <a
-                      key={index}
-                      href={social.url}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className={`text-neutral-300 transition-transform duration-300 hover:scale-125  ${
-                        index === 0 && "hover:text-blue-600"
-                      } 
-            ${index === 1 && "hover:text-pink-500"} 
-            ${index === 2 && "hover:text-sky-400"} 
-            ${index === 3 && "hover:text-blue-700"}
-            ${index === 4 && "hover:text-green-600"}`}
+                      href={link.path}
+                      className="text-neutral-400 text-sm tracking-wide"
                     >
-                      {social.icon}
+                      {link.title}
                     </a>
-                  ))}
-                </div>
-              </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+          <div className="flex flex-col lg:justify-evenly justify-evenly ">
+            {/* Get Started */}
+            <GetStarted />
+            <div className="flex">
+              {/* Social Media */}
+              <Socials />
             </div>
           </div>
         </div>
--- src/components/Footer/GetStarted.jsx
+++ src/components/Footer/GetStarted.jsx
@@ -4,25 +4,22 @@ import { Button } from "../ui/button";
 
 const GetStarted = () => {
   return (
-    <div className=" space-y-4">
-      <Label
-        htmlFor="email"
-        className="text-md font-semibold text-neutral-500 tracking-tight"
-      >
-        Try Elysium VR for free
+    <div className=" w-full space-y-2 lg:space-y-5">
+      <Label htmlFor="email" className="text-neutral-500">
+        Try Elysium VR for Free
       </Label>
-      <div className="border rounded-md border-neutral-700/80">
+      <div className="lg:relative flex flex-col">
         <Input
+          className="bg-foreground border border-neutral-700/80 rounded-md w-full lg:w-[40vh] "
           type="email"
+          id="email"
           placeholder="Enter your email"
-          className="bg-foreground lg:w-full lg:border-none w-full"
         />
-        <Button
-          className="absolute flex bg-orange-600 rounded-md rounded-l-none top-10 lg:right-0 right-0"
-          type="submit"
-        >
-          Get Started
-        </Button>
+        <div className="hidden lg:flex lg:absolute top-0 right-0">
+          <Button variant="gradient" className="rounded-l-none">
+            Get Started
+          </Button>
+        </div>
       </div>
     </div>
   );
