fa52e10f806475b5dd91759e66dc8c03600d383e
kramislife <egariando619@gmail.com>
2024-10-09 16:33:22 +0800

    Refactor Footer/Reserved component layout

--- src/components/Footer/Reserved.jsx
+++ src/components/Footer/Reserved.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const Reserved = () => {
   return (
-    <div className="flex justify-between items-center lg:mx-14 lg:px-0 px-4 py-4 border-t border-neutral-800/90">
-    <p className="text-neutral-500 text-xs tracking-wide">
-      &copy; 2024 Elysium VR. All rights reserved.
-    </p>
-    <div className="flex items-center space-x-2">
-      <a href="#" className="text-neutral-500 text-xs tracking-wide">
-        Privacy Policy
-      </a>
-      <span className="text-neutral-500"> • </span>
-      <a href="#" className="text-neutral-500 text-xs tracking-wide">
-        Terms of Service
-      </a>
-    </div>
-  </div>
-  )
-}
+    <div className="flex lg:mx-14 lg:px-0 px-3 py-4 border-t border-neutral-800/90">
+      <div className="flex justify-center lg:justify-between w-full">
+        <p className="text-neutral-500 text-xs tracking-wide">
+          &copy; 2024 Elysium VR. All rights reserved.
+        </p>
 
-export default Reserved
+        <div className="hidden lg:flex items-center lg:gap-2">
+          <a href="#" className="text-neutral-500 text-xs tracking-wide">
+            Privacy Policy
+          </a>
+          <span className="text-neutral-500"> • </span>
+          <a href="#" className="text-neutral-500 text-xs tracking-wide">
+            Terms of Service
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Reserved;
